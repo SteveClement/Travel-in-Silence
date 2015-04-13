dd6c3473360cf9eb60a7fd455f22430e8873f23f
Steve Clement <steve@localhost.lu>
2015-04-13 18:47:31 +0200

    - Removed Samples - Added Pause Button

--- Travel in Silence/GameScene.swift
+++ Travel in Silence/GameScene.swift
@@ -9,34 +9,18 @@
 import SpriteKit
 
 class GameScene: SKScene {
+
+    let pauseButton = SKSpriteNode(imageNamed: "pause")
+
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!";
-        myLabel.fontSize = 65;
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
-        
-        self.addChild(myLabel)
+        self.pauseButton.position = CGPointMake(CGRectGetMaxX(self.frame), CGRectGetMaxY(self.frame))
+        self.addChild(self.pauseButton)
     }
     
     override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
         /* Called when a touch begins */
         
-        for touch in (touches as! Set<UITouch>) {
-            let location = touch.locationInNode(self)
-            
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
-            
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
-            
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-            
-            sprite.runAction(SKAction.repeatActionForever(action))
-            
-            self.addChild(sprite)
-        }
     }
    
     override func update(currentTime: CFTimeInterval) {
