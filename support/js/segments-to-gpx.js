f33d54fd8b01dcd165c12cf04df4e6cb6c603960
Steve Clement <steve@localhost.lu>
2015-04-29 16:40:34 +0200

    - Updated l10segments show wrong segment - Added real-world track Mersch-Lux and back

--- support/js/segments-to-gpx.js
+++ support/js/segments-to-gpx.js
@@ -21,7 +21,7 @@ var l70segments = [ 6, 7, 71, 72 ]
 var l80segments = [ 74, 5, 44 ]
 var l90segments = [ 3 ]
 
-var active = l50segments
+var active = l10segments
 
 Object.keys(gaLigneInfo).forEach(function (segment) {
     if ($.inArray(parseInt(segment.match(/\d+/)[0]), active) != -1) {
